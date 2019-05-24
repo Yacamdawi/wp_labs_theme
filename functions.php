88881f508e015ff4f3b3f061d020d564282a28ab
Yacamdawi <yacin-hamdaoui@hotmail.com>
2019-05-24 18:34:09 +0200

    Functions créer pour les str_replace lors de l'utilisations des customizers

--- functions.php
+++ functions.php
@@ -6,3 +6,37 @@ require_once(INCLUDE_DIR . '/enqueue_scripts.php');
 require_once(INCLUDE_DIR . '/menu.php');
 require_once(INCLUDE_DIR . '/sidebar.php');
 require_once(INCLUDE_DIR . '/customizer.php');
+
+function strongTitle($settingKey)
+{
+    $titre = get_theme_mod($settingKey);
+    $titre = str_replace("[", "<span>", $titre);
+    $titre = str_replace("]", "</span>", $titre);
+
+    return $titre;
+}
+
+function lineBreak($settingKey)
+{
+    $titre = get_theme_mod($settingKey);
+    $titre = str_replace("#", "</br>", $titre);
+
+    return $titre;
+}
+
+function wpb_change_title_text($title)
+{
+    $screen = get_current_screen();
+    if ('service' == $screen->post_type) {
+        $title = 'Entrez le nom du service';
+    } else if ('feature' == $screen->post_type) {
+        $title = 'Entrez le nom du projet';
+    } else if ('client' == $screen->post_type) {
+        $title = 'Entrez le nom du client';
+    } else if ('team' == $screen->post_type) {
+        $title = 'Entrez le nom du collaborateur';
+    }
+    return $title;
+}
+
+add_filter('enter_title_here', 'wpb_change_title_text');
